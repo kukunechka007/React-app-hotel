e711e51c9d639491d4e52fcb2ad9d10866961a7d
Kate_M <eamyskina@gmail.com>
2025-01-24 23:40:17 +0100

    initial level of project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+const App = () => {
+  return (
+    <BrowserRouter>
+       React-app-hotel
+    </BrowserRouter>
+  )
+}
+
+export default App
